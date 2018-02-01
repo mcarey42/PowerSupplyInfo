a3139a994b03102a5f6fe566a7f97af2a8caaf5c
Mark Carey <mcarey@solcore.net>
2018-02-01 15:19:22 -0500

    Initial Commit

---
+++ PowerSupplyInfo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PowerSupplyInfo
+//
+//  Created by Mark Carey on 2/1/18.
+//  Copyright © 2018 Mark Carey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
